b52e0486fa8ea23bd0009c3382af962c314442fe
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2021-11-24 14:07:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var request = new XMLHttpRequest(); 
+
+request.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOURKEY");
+
+request.onload = function() {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	console.log(parsedData);
+}
+
+request.send();
