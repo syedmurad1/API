453ef1602043f92f99c1d31a9685593ca8468057
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2021-11-29 16:45:14 +0000

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest();
+
+request.open('GET',"URL with KEY");
+
+request.onload = function() {
+	var obj =JSON.parse(this.response);
+	console.log(obj);
+	document.getElementById('name').innerHTML = obj.name;
+	document.getElementById('weather').innerHTML = obj.weather[0].description;
+	document.getElementById('temp').innerHTML = obj.main.temp;
+};
+request.send();
